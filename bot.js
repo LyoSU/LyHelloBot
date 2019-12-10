4a99b3e1d62b66a9212540a9d3861ea3a032655e
LyoSU <mail@lyo.su>
2019-12-10 21:03:51 +0200

    add polling

--- bot.js
+++ bot.js
@@ -3,9 +3,17 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 async function main () {
-  bot.telegram.sendMessage(66478514, 'Hello!')
+  bot.telegram.sendMessage(66478514, 'Hello from GitHub Actions!')
 }
 
 ;(async () => {
   await main()
 })()
+
+bot.use(async (ctx, next) => {
+  ctx.reply('Hello from GitHub Actions!')
+})
+
+bot.launch().then(() => {
+  console.log('bot start polling')
+})
